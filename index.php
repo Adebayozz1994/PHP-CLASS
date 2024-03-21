cc807aef59ae1205ecdb77fddd90bd36aa630d63
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-03-21 11:58:47 -0100

    start php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello World";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>ADEBAYOZZ</h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur dignissimos, molestiae libero, natus sequi commodi nesciunt at aut odio dolorem magni fuga numquam quasi veniam tempore animi, voluptatem deserunt a veritatis deleniti totam! Dolorum, exercitationem at. Dicta molestiae expedita doloremque dolores earum modi amet eaque inventore sequi, ut perspiciatis veniam.
+    </div>
+</body>
+</html>
